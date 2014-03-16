a1e52009c1e530462c86d0d620b2a7faff84bc86
Kemal Dağ <gkemaldag@gmail.com>
2014-03-16 02:23:54 +0200

    host name updated

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express();
 app.use(express.vhost('kemald.com',  require('./app.js').app));
-app.use(express.vhost('subapi-kemald.rhcloud.com',  require('./app.js').app));
+app.use(express.vhost('supapi-kemald.rhcloud.com',  require('./app.js').app));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
+console.log(ipaddress);
 var server = app.listen(port,ipaddress, function() {
     console.log('Listening on port %d', server.address().port); 
 }); 
