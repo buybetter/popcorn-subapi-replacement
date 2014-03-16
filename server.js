8d8e69b7a18828b61828f6ae3ba29680ff7b957c
Kemal Dağ <gkemaldag@gmail.com>
2014-03-16 16:14:15 +0200

    mongoose added

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var app = require('./app.js').app;
 
 var port = 8080;
 
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
+
+
 server.use(express.vhost('*',  app));
 
 var ret = server.listen(port, function() {
