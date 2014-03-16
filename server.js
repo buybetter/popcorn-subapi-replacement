99fd84d19b749a9a960d6cb8c2b01fc81e9cf569
Kemal Dağ <gkemaldag@gmail.com>
2014-03-16 10:53:13 +0200

    server.listen compatible with openshift

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 server.use(express.vhost('*',  app));
 
-var ret = server.listen(port, function() {
-    console.log('Listening on port %d a', ret.address().port); 
-}); 
+if(process.env.OPENSHIFT_NODEJS_IP === undefined){
+	var ret = server.listen(port, function() {
+    	console.log('Listening on port %d a', ret.address().port); 
+	}); 	
+}else{
+	var ret = server.listen(port, process.env.OPENSHIFT_NODEJS_IP, function() {
+    	console.log('Listening on port %d a', ret.address().port); 
+	}); 
+}
