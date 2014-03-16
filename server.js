4e860a22687bc8ebdd390aceb30dbe2fad3a33af
Kemal Dağ <gkemaldag@gmail.com>
2014-03-16 12:47:46 +0200

    openshift related code removed

--- server.js
+++ server.js
@@ -3,16 +3,10 @@ var express = require('express'),
 
 var app = require('./app.js').app;
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = 8080;
 
 server.use(express.vhost('*',  app));
 
-if(process.env.OPENSHIFT_NODEJS_IP === undefined){
-	var ret = server.listen(port, function() {
-    	console.log('Listening on port %d a', ret.address().port); 
-	}); 	
-}else{
-	var ret = server.listen(port, process.env.OPENSHIFT_NODEJS_IP, function() {
-    	console.log('Listening on port %d nodejitsu', ret.address().port); 
-	}); 
-} 
+var ret = server.listen(port, function() {
+	console.log('Listening on port %d nodejitsu', ret.address().port); 
+}); 	
